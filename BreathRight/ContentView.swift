926291704ac9315ec58115d52f2954a948344ea5
codemiester3000 <owenkamilkhoury@gmail.com>
2023-09-30 19:59:22 -0400

    initial commit

--- BreathRight/ContentView.swift
+++ BreathRight/ContentView.swift
@@ -1,90 +1,161 @@
-//
-//  ContentView.swift
-//  BreathRight
-//
-//  Created by Owen Khoury on 9/30/23.
-//
-
 import SwiftUI
-import CoreData
+
+extension Color {
+    static let robinhoodGreen = Color(#colorLiteral(red: 0.1803921569, green: 0.8, blue: 0.4431372549, alpha: 1))
+}
+
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
-
+    
+    @State private var isDragging: Bool = false
+    
+    let minDuration: CGFloat = 2
+    let maxDuration: CGFloat = 16
+    
+    
+    @State private var sliderValue: CGFloat
+    
+    
+    let minSquareSize: CGFloat = 50
+    let maxSquareSize: CGFloat = 250
+    
+    var durationInSeconds: Int {
+        Int(minDuration + (maxDuration - minDuration) * sliderValue)
+    }
+    
+    init() {
+        let initialValue = (4 - minDuration) / (maxDuration - minDuration)
+        _sliderValue = State(initialValue: initialValue)
+        
+        for family: String in UIFont.familyNames {
+            print("\(family)")
+            for names: String in UIFont.fontNames(forFamilyName: family) {
+                print("== \(names)")
+            }
+        }
+    }
+    
     var body: some View {
-        NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
+        VStack(spacing: 0) {
+            
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("Box Breathing")
+                        .font(.custom("Inter-Variable", size: 30))
+                    Text("Configure your settings and press start to begin")
+                        .font(.custom("Inter-Variable", size: 15))
+                        .multilineTextAlignment(.leading)
+                        .padding(.top, 4)
+                }
+                Spacer()
+            }
+            .padding(.top, 50)
+            .padding(.horizontal, 20)
+            
+            Spacer()
+            
+            VStack {
+                ZStack {
+                    Rectangle()
+                        .stroke(Color.robinhoodGreen, lineWidth: 4)
+                        .frame(width: sizeForSquare, height: sizeForSquare)
+                        .animation(isDragging ? .none : .easeInOut(duration: 0.5))
+                    
+                    Text("\(durationInSeconds) sec")
+                        .offset(y: -(sizeForSquare / 2 + 20))
+                    
+                    Text("\(durationInSeconds)")
+                        .offset(y: sizeForSquare / 2 + 20)
+                    
+                    Text("\(durationInSeconds)")
+                        .offset(x: -(sizeForSquare / 2 + 20))
+                    
+                    Text("\(durationInSeconds)")
+                        .offset(x: sizeForSquare / 2 + 20)
+                }
+            }
+            .frame(height: sizeForSquare)
+            .padding(.top, 20)
+            
+            
+            Spacer()
+            
+            VStack(alignment: .leading) {
+                HStack {
+                    CustomSlider(value: $sliderValue, isDragging: $isDragging)
+                }
+                .padding(.horizontal, 20)
+                .frame(height: 40)
+                
+                HStack {
+                    Button(action: {}) {
+                        Text("Begin Now")
+                            .font(.custom("Inter-Variable", size: 15))
+                            .padding()
+                            .background(Color.robinhoodGreen)
+                            .foregroundColor(.white)
+                            .cornerRadius(10)
                     }
                 }
-                .onDelete(perform: deleteItems)
+                .padding(.horizontal, 20)
+                .padding(.top, 10)
             }
-            .toolbar {
-#if os(iOS)
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-#endif
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-            Text("Select an item")
+            .padding(.bottom, 125)
         }
+        .background(
+            LinearGradient(gradient: Gradient(colors: [Color.gray.opacity(0.4), Color.gray.opacity(0.2)]), startPoint: .top, endPoint: .bottom)
+                .edgesIgnoringSafeArea(.all)
+        )
     }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
+    
+    
+    
+    var sizeForSquare: CGFloat {
+        minSquareSize + (maxSquareSize - minSquareSize) * sliderValue
     }
+    
+}
 
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+struct CustomSlider: View {
+    @Binding var value: CGFloat
+    @Binding var isDragging: Bool
+    let trackColor = Color.gray.opacity(0.2)
+    let thumbColor = Color.robinhoodGreen
+    let sliderWidth: CGFloat = UIScreen.main.bounds.width - 40
+    let thumbSize: CGFloat = 30
+    
+    var body: some View {
+        GeometryReader { geometry in
+            ZStack(alignment: .leading) {
+                RoundedRectangle(cornerRadius: 15)
+                    .frame(width: geometry.size.width, height: 5)
+                    .foregroundColor(trackColor)
+                
+                RoundedRectangle(cornerRadius: 15)
+                    .frame(width: geometry.size.width * value, height: 5)
+                    .foregroundColor(thumbColor)
+                
+                Circle()
+                    .frame(width: thumbSize, height: thumbSize)
+                    .foregroundColor(thumbColor)
+                    .position(x: geometry.size.width * value, y: geometry.size.height / 2)
+                    .gesture(
+                        DragGesture().onChanged { gesture in
+                            self.value = min(max(gesture.location.x / geometry.size.width, 0), 1)
+                            self.isDragging = true  // user has started dragging
+                        }.onEnded { _ in
+                            self.isDragging = false  // user has ended dragging
+                        }
+                    )
             }
         }
+        .frame(width: sliderWidth)
     }
 }
 
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+        ContentView()
     }
 }
