a97370df836b5ae73330e91c3cf6d037d0f2b763
codemiester3000 <owenkamilkhoury@gmail.com>
2023-10-08 23:57:36 -0400

    deep green color for buttons

--- BreathRight/ContentView.swift
+++ BreathRight/ContentView.swift
@@ -2,7 +2,8 @@ import SwiftUI
 import AVFoundation
 
 extension Color {
-    static let robinhoodGreen = Color(#colorLiteral(red: 0.1803921569, green: 0.8, blue: 0.4431372549, alpha: 1))
+    static let robinhoodGreen = Color(hex: "3CB371") // Color(#colorLiteral(red: 0.1803921569, green: 0.8, blue: 0.4431372549, alpha: 1))
+    static let deepGreen = Color(hex: "2E8B57")
 }
 
 
@@ -184,7 +185,7 @@ struct ContentView: View {
                             }
                             .font(.custom("Inter-Variable", size: 20))
                             .padding()
-                            .background(Color.gray.opacity(0.8))
+                            .background(Color.deepGreen)
                             .foregroundColor(.white)
                             .cornerRadius(10)
                             
@@ -213,7 +214,7 @@ struct ContentView: View {
                             }
                             .font(.custom("Inter-Variable", size: 20))
                             .padding()
-                            .background(Color.gray.opacity(0.8))
+                            .background(Color(hex: "2E8B57"))
                             .foregroundColor(.white)
                             .cornerRadius(10)
                         }
@@ -421,5 +422,33 @@ struct CustomSlider: View {
             }
         }
         .frame(width: sliderWidth)
+    }
+}
+
+extension Color {
+    init(hex: String) {
+        let hex = hex.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
+        var int: UInt64 = 0
+        
+        Scanner(string: hex).scanHexInt64(&int)
+        let a, r, g, b: UInt64
+        switch hex.count {
+        case 3: // RGB (12-bit)
+            (a, r, g, b) = (255, (int >> 8) * 17, (int >> 4 & 0xF) * 17, (int & 0xF) * 17)
+        case 6: // RGB (24-bit)
+            (a, r, g, b) = (255, int >> 16, int >> 8 & 0xFF, int & 0xFF)
+        case 8: // ARGB (32-bit)
+            (a, r, g, b) = (int >> 24, int >> 16 & 0xFF, int >> 8 & 0xFF, int & 0xFF)
+        default:
+            (a, r, g, b) = (255, 0, 0, 0)
+        }
+        
+        self.init(
+            .sRGB,
+            red: Double(r) / 255,
+            green: Double(g) / 255,
+            blue:  Double(b) / 255,
+            opacity: Double(a) / 255
+        )
     }
 }
